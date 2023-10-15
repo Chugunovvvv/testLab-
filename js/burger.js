bafac0b8bab2881194441d510ed71f4784d91dd9
Chugunovvvv <cugunovroman03@gmail.com>
2023-10-15 22:20:56 +0300

    fix bags

--- js/burger.js
+++ js/burger.js
@@ -2,10 +2,11 @@ const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.header__body');
 const body = document.body;
 const link = document.querySelectorAll('.header__menu-item');
-
+const logoText = document.querySelector('.header__logo-text');
 burger.addEventListener('click', (e) => {
     menu.classList.toggle('active');
     burger.classList.toggle('header__burger--active');
+    logoText.classList.toggle('active-text');
     body.classList.toggle('lock');
    
 })
@@ -14,6 +15,7 @@ link.forEach(el => {
     el.addEventListener('click', () => {
         menu.classList.remove('active');
     burger.classList.remove('header__burger--active');
+    logoText.classList.remove('acitve-text');
     body.classList.remove('lock');
     })
 });
