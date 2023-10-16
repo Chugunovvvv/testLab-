2f7bd5ccdb1937e3886d411d6bd694f4b92413ae
Chugunovvvv <cugunovroman03@gmail.com>
2023-10-16 23:45:54 +0300

    add validation

--- js/validate.js
+++ js/validate.js
@@ -2,3 +2,17 @@
 const telSelector = form.querySelector('input[type="tel"]');
 const inputMask = new Inputmask('+7 (999) 999-99-99');
 inputMask.mask(telSelector);
+
+const error = document.getElementById('error');
+const inputName = document.getElementById('contact-name');
+function validateName() {
+    const name = inputName.value;
+    if(name.length == 0 || name.length < 3) {
+        error.innerHTML = 'Введите корректное имя';
+        inputName.style.border = '2px solid red'
+        return false;
+    }
+    error.innerHTML = '';
+    inputName.style.border = ''
+    return true;
+}
